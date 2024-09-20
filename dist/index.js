ef9ec4a562a134fda1c823bd7fabb6d4f468a753
Haitao Chen <haitch@microsoft.com>
2024-09-20 10:25:05 -0700

    proper output

--- dist/index.js
+++ dist/index.js
@@ -26830,7 +26830,28 @@ tokens.forEach((token, index) => {
     if (index >= 2) {
         return;
     }
-    core.info(JSON.stringify(JSON.parse(atob(token)), null, 4));
+    if (index === 0) {
+        core.info("Header:");
+        core.info(JSON.stringify(JSON.parse(atob(token)), null, 4));
+        return;
+    }
+
+    core.info("Payload:");
+    const claims = JSON.parse(atob(token))
+    core.info(JSON.stringify(claims, null, 4));
+    if (claims.iss) {
+        core.setOutput('issuer', claims.iss);
+    }
+    if (claims.sub) {
+        core.setOutput('subject', claims.sub);
+    }
+    if (claims.aud) {
+        core.setOutput('audience', claims.aud);
+    }
+
+    Object.entries(claims).forEach((key, value) => {
+        core.setOutput(`claim-${key}`, JSON.stringify(value));
+    });
 });
 })();
 
