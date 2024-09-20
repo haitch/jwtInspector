df02cf9872fdbe72c9777b3c6eda7d38e2e23162
Haitao Chen <haitch@microsoft.com>
2024-09-20 10:07:00 -0700

    script tweaks

--- dist/index.js
+++ dist/index.js
@@ -26827,8 +26827,10 @@ const token = core.getInput('token')
 const tokens = token.split(".");
 
 tokens.forEach((token, index) => {
-    core.info(`token part ${index + 1}: ${token}`);
-    core.info(atob(token));
+    if (index >= 2) {
+        return;
+    }
+    core.info(JSON.stringify(JSON.parse(atob(token)), null, 4));
 });
 })();
 
